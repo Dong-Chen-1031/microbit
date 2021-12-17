53f74cf33c50be54167380a7546ad15774c55874
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2021-12-17 21:10:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,41 @@
+// 5
+// 10
+// 1
+let 熱圾車 = [
+659,
+622,
+659,
+622,
+659,
+494,
+587,
+523,
+220,
+330,
+440,
+262,
+330,
+440,
+494,
+659,
+330,
+494,
+659
+]
+let 熱射車圖 = images.createBigImage(`
+    . # # . # # # # . .
+    # . # . # . . . # .
+    # # # # # . . . . #
+    # # # # # # # # # #
+    . # # . . . # # . .
+    `)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    for (let index = 0; index <= 熱圾車.length - 1; index++) {
+        music.playTone(熱圾車[index], music.beat(BeatFraction.Half))
+        熱射車圖.showImage(-5 + index, 0)
+    }
+    basic.clearScreen()
+})
